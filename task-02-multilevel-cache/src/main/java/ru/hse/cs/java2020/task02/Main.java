88324499f07967e88804a06b26f62eecada59145
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2020-03-09 03:03:14 +0300

    Replace main with example

--- task-02-multilevel-cache/src/main/java/ru/hse/cs/java2020/task02/Main.java
+++ task-02-multilevel-cache/src/main/java/ru/hse/cs/java2020/task02/Main.java
@@ -1,62 +1,23 @@
 package ru.hse.cs.java2020.task02;
 
-import java.util.Scanner;
+import java.nio.file.Files;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final int argsCount = 4;
-        final int cacheDirPos = 0;
-        final int memorySizePos = 1;
-        final int diskSizePos = 2;
-        final int policyPos = 3;
-
-        if (args.length != argsCount) {
-            System.err.println("Usage: ./cache /path/to/cache/dir <memorySize> <diskSize> <policy>");
-            return;
-        }
-
-        String cacheDir = args[cacheDirPos];
-        long memorySize = Long.parseLong(args[memorySizePos]);
-        long diskSize = Long.parseLong(args[diskSizePos]);
-
-        EvictionPolicy<Long> policy;
-        switch (args[policyPos]) {
-            case "LRU":
-                policy = new LRUPolicy.Builder<>();
-                break;
-            case "LFU":
-                policy = new LFUPolicy.Builder<>();
-                break;
-            default:
-                System.err.println("Only LFU and LRU policies are supported!");
-                return;
-        }
+        final String cacheDir = Files.createTempDirectory("cacheMain").toString();
+        final long memorySize = 1024 * 1024;
+        final long diskSize = 1024 * 1024 * 100;
+        final EvictionPolicy<Long> policy = new LFUPolicy.Builder<>();
 
         try (CacheImpl cache = new CacheImpl(memorySize, diskSize, cacheDir, policy)) {
-            String action;
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                action = scanner.next();
-
-                switch (action) {
-                    case "exit":
-                        return;
-
-                    case "put":
-                        Long key = scanner.nextLong();
-                        String value = scanner.nextLine();
-                        System.out.printf("Previous value: %s\n", cache.put(key, value));
-                        break;
-
-                    case "get":
-                        key = scanner.nextLong();
-                        System.out.printf("Get %d = %s\n", key, cache.get(key));
-                        break;
-
-                    default:
-                        System.err.println("Unknown operation, please enter one of [exit, put, get]");
-                }
-            }
+            System.out.println(cache.get(0L));
+            System.out.println(cache.put(0L, ""));
+            System.out.println(cache.put(0L, "abacabadaba"));
+            System.out.println(cache.put(0L, "test"));
+            System.out.println(cache.get(0L));
+            System.out.println(cache.put(1L, "string"));
+            System.out.println(cache.put(1L, "strong"));
+            System.out.println(cache.get(1L));
         }
     }
 }
